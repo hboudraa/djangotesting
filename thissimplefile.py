ddd072487c131fc0ad04695ddf4c4e4ed84e2a16
hboudraa <bdrnet94@gmail.com>
2023-08-17 12:44:14 +0100

    add new line

--- thissimplefile.py
+++ thissimplefile.py
@@ -1,2 +1,4 @@
 print('this is a simple file')
 print('this is from hakim')
+print('this line of code created from hboudraa after "git add name_of_file"')
+print('in this line we set github config')
